d3d0ae25f3488c359eecb6e532a2462c341ed495
XHN <XHN@NTBK554881>
2019-08-01 18:15:31 +0800

    update edit

--- ProjectForecast-OA/Client/src/views/Project/project-detail.vue
+++ ProjectForecast-OA/Client/src/views/Project/project-detail.vue
@@ -42,11 +42,6 @@
                                                 </el-input>
                                             </template>
                                         </el-table-column>
-                                        <el-table-column prop="Month" label="Month" style="width:6vw;">
-                                            <template scope="scope">
-                                                <el-input size="mini" v-model="scope.row.Month" disabled="disabled"></el-input>
-                                            </template>
-                                        </el-table-column>
                                         <el-table-column prop="Type" label="Type">
                                             <template scope="scope">
                                                 <el-input size="mini" v-model="scope.row.Type" disabled="disabled"></el-input>
@@ -57,11 +52,11 @@
                                                 <el-input size="mini" v-model="scope.row.CostRate" disabled="disabled"></el-input>
                                             </template>
                                         </el-table-column>
-                                        <el-table-column prop="CostRate" label="WorkDays" disabled="disabled">
-                                            <template scope="scope">
-                                                <el-input size="mini" v-model="scope.row.WorkDays"></el-input>
-                                            </template>
-                                        </el-table-column>
+                                            <el-table-column prop="MonthElement" :label="MonthMapping(month)" v-for='(month,index) in months'>
+                                                    <template scope="scope">
+                                                        <el-input size="mini" v-model="scope.row.MonthElement[index].WorkingHour" disabled="disabled"></el-input>
+                                                    </template>
+                                                </el-table-column>
                                     </el-table>
                             <el-button type="primary">Save</el-button>
 
@@ -76,22 +71,22 @@
                                     </el-table-column>
                                     <el-table-column prop="Revenue" label="Revenue" style="width:6vw;">
                                         <template scope="scope">
-                                            <el-input size="mini" v-model="scope.row.Revenue"></el-input>
+                                            <el-input size="mini" v-model="scope.row.Revenue" disabled="disabled"></el-input>
                                         </template>
                                     </el-table-column>
                                     <el-table-column prop="Expenses" label="Expenses">
                                         <template scope="scope">
-                                            <el-input size="mini" v-model="scope.row.Expenses"></el-input>
+                                            <el-input size="mini" v-model="scope.row.Expenses" disabled="disabled"></el-input>
                                         </template>
                                     </el-table-column>
                                     <el-table-column prop="CostRate" label="IT(HW/SW)">
                                         <template scope="scope">
-                                            <el-input size="mini" v-model="scope.row.IT"></el-input>
+                                            <el-input size="mini" v-model="scope.row.IT" disabled="disabled"></el-input>
                                         </template>
                                     </el-table-column>
                                     <el-table-column prop="Materials" label="Materials">
                                         <template scope="scope">
-                                            <el-input size="mini" v-model="scope.row.Materials"></el-input>
+                                            <el-input size="mini" v-model="scope.row.Materials" disabled="disabled"></el-input>
                                         </template>
                                     </el-table-column>                                 
                                 </el-table>
@@ -103,6 +98,7 @@
       </template>
       <script>
         var formData_service = require('./../../services/form-data-service');
+        var moment = require('moment');
         export default {
           data() {
             return {
@@ -113,7 +109,12 @@
               loading: false,       
               projects:[],
               projectFinance:[],
-              FinancialReport:[]
+              FinancialReport:[],
+              months: [],
+              timeSpan:[],
+              startMonth:0,
+              endMonth:0,
+              FinancialReportMonths: [],
             }
           },
           created() {
@@ -124,17 +125,67 @@
               .then(data => {
                 _vm.detailData = data.data[0];
                 _vm.form=_vm.detailData;
-                _vm.infiledList=_vm.detailData.Employees;
+                _vm.timeSpan = [_vm.form.StartDate, _vm.form.CloseDate];
+                // _vm.infiledList=_vm.detailData.Employees;
                 _vm.FinancialReport=_vm.detailData.ProjectFinancList;
-                // _vm.treeList.forEach(element => {
-                //   let tree={"Type":element.Type,"Status":element.Status,"Country":element.Country,"ProjectNo":element.ProjectNo,"Customer":element.Customer,"ProjectName":element.ProjectName,"CloseDate":element.CloseDate}
-                //   element.ProjectFinancList.forEach(project=>{
-                //     let monthTemp=_vm.MonthMapping(project.Month);
-                //     tree[monthTemp]=project.Revenue;
-                //   });
-                //   _vm.projectFinance.push(tree);
-                // });
-                // this.projectFinance=
+                _vm.startMonth = parseInt(moment(this.timeSpan[0]).format('MM-DD-YYYY').split('-')[0]);
+                        _vm.endMonth = parseInt(moment(this.timeSpan[1]).format('MM-DD-YYYY').split('-')[0]);
+for(let i=_vm.startMonth;i<=_vm.endMonth;i++){
+    this.months.push(i);
+}
+                let result = _vm.groupBy(_vm.detailData.Employees, function (item) {
+                            return [item.Consultant_Id];
+                        })
+
+                        result.forEach(employee => {
+                            let consultant_show = { Id: employee[0].Id, Consultant_Id: employee[0].Consultant_Id, Consultant_Name: employee[0].Consultant_Name, Type: employee[0].Type, CostRate: employee[0].CostRate, ProjectNo: this.form.ProjectNo, Month: employee[0].Month, };
+                            let MonthElement = [];
+                            employee.forEach(monthElement => {
+                                let exits = false;
+                                for (let j in _vm.months)
+                                {
+                                    if (_vm.months[j] == _vm.MonthMappingReverse(monthElement.Month)) {
+                                        exits = true;
+                                        if((_vm.getProperty(MonthElement,'Month', _vm.MonthMapping(_vm.months[j])))&&(_vm.getProperty(MonthElement,'Month', _vm.MonthMapping(_vm.months[j])).WorkingHour==0))
+                                        {
+                                            MonthElement.forEach(a=>{
+                                                if(a.Month==monthElement.Month)
+                                                {
+                                                    a.WorkingHour=monthElement.WorkDays;
+                                                }
+                                            })
+                                        
+                                        }
+                                        else if(!(_vm.getProperty( MonthElement,'Month', _vm.MonthMapping(_vm.months[j]))))
+                                        {
+                                            MonthElement.push({ Month: monthElement.Month, WorkingHour: monthElement.WorkDays })
+                                        }
+                                    }
+                                    else{
+                                        if(!(_vm.getProperty( MonthElement,'Month', _vm.MonthMapping(_vm.months[j]))))
+                                        {
+                                            MonthElement.push({ Month: _vm.MonthMapping(_vm.months[j]), WorkingHour: 0})
+                                        }
+                                        
+                                    }
+
+                                }
+                                // if (!exits) {
+                                //     // _vm.months.push(_vm.MonthMappingReverse(monthElement.Month));
+                                //     MonthElement.push({ Month: _vm.months[j], WorkingHour: 0})
+                                // }
+                                // else{
+                                //     MonthElement.push({ Month: monthElement.Month, WorkingHour: monthElement.WorkDays })
+                            
+                                // }
+
+                               
+
+                            });
+                            consultant_show.MonthElement = MonthElement;
+                            _vm.infiledList.push(consultant_show);
+                        });
+                        _vm.months.sort();
                 _vm.loading=false;
                 console.log(_vm.form);
               });
@@ -142,30 +193,59 @@
       
           },
           methods: {
-            MonthMapping(month){
-      switch(month){
-        case "1":return "Mon";
-        case "2":return "Feb";
-        case "3":return "Mar";
-        case "4":return "Apr";
-        case "5":return "May";
-        case "6":return "Jun";
-        case "7":return "Jul";
-        case "8":return "Aug";
-        case "9":return "Sep";
-        case "10":return "Oct";
-        case "11":return "Sep";
-        case "12":return "Dec";
-      }
+            MonthMapping(month) {
+                switch (month) {
+                    case 1: return "Mon";
+                    case 2: return "Feb";
+                    case 3: return "Mar";
+                    case 4: return "Apr";
+                    case 5: return "May";
+                    case 6: return "Jun";
+                    case 7: return "Jul";
+                    case 8: return "Aug";
+                    case 9: return "Sep";
+                    case 10: return "Oct";
+                    case 11: return "Nov";
+                    case 12: return "Dec";
+                }
             },
             NextPage(){
                 this.activeName='second';
             },
-            handleNodeClick(data) {
-              console.log(data);
+            getProperty(list, property, value) {
+                for (let i in list) {
+                    if (list[i][property] == value) {
+                        return list[i];
+                    }
+                }
+
             },
-            handleClick(tab, event) {
-              console.log(tab, event);
+            groupBy(array, f) {
+                let groups = {};
+                array.forEach(function (o) {
+                    let group = JSON.stringify(f(o));
+                    groups[group] = groups[group] || [];
+                    groups[group].push(o);
+                });
+                return Object.keys(groups).map(function (group) {
+                    return groups[group];
+                });
+            },
+            MonthMappingReverse(month) {
+                switch (month) {
+                    case "Mon": return 1;
+                    case "Feb": return 2;
+                    case "Mar": return 3;
+                    case "Apr": return 4;
+                    case "May": return 5;
+                    case "Jun": return 6;
+                    case "Jul": return 7;
+                    case "Aug": return 8;
+                    case "9": return 9;
+                    case "Oct": return 10;
+                    case "Nov": return 11;
+                    case "Dec": return 12;
+                }
             },
            
           }
